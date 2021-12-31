5fd77355f911361b9e002b66ab2464d00d82dba9
Spartalis <peter.spartalis@gmail.com>
2021-12-31 07:44:19 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".navbar-burger");
+  burger.addEventListener("click", () => {
+    const target = burger.dataset.target;
+    const $target = document.getElementById(target);
+    burger.classList.toggle("is-active");
+    $target.classList.toggle("is-active");
+  });
+});
