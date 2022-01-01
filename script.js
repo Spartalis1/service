042e1cd1e2f4661480ac6c5e6072b64ae766ed80
Spartalis <peter.spartalis@gmail.com>
2021-12-31 18:23:28 -0800

    Fixed Hero

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
     $target.classList.toggle("is-active");
   });
 });
-const startButtons = document.querySelectorAll(".start");
+//CARD Animation
+const cards3 = document.querySelectorAll(".c3");
+const c1 = document
+  .querySelector(".c1")
+  .childNodes[1].querySelectorAll(".toggle");
+console.log(c1);
+cards3.forEach((card) =>
+  card.addEventListener("mouseenter", (e) => {
+    let tog = e.target.childNodes[1].querySelectorAll(".toggle");
+    tog.forEach((el) => el.classList.remove("is-hidden"));
+    c1.forEach((el) => el.classList.add("is-hidden"));
+  })
+);
+cards3.forEach((card) =>
+  card.addEventListener("mouseleave", (e) => {
+    let tog = e.target.childNodes[1].querySelectorAll(".toggle");
+    tog.forEach((el) => el.classList.add("is-hidden"));
+    c1.forEach((el) => el.classList.remove("is-hidden"));
+  })
+);
